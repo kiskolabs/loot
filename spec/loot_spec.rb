74fc564ba4bf5b60fd09943c766e8215335ba3a2
Matias Korhonen <matias@kiskolabs.com>
2012-05-09 22:04:13 +0300

    Use let.

--- spec/loot_spec.rb
+++ spec/loot_spec.rb
@@ -1,8 +1,31 @@
 require "spec_helper"
 
 describe Loot do
-  def app
-    @app ||= Loot
+  let(:app) { Loot }
+  let(:single_ticket_params) do
+    {
+      "eventId" => "56789012",
+      "eventIdentifier" => "frozenrails-2012",
+      "eventType" => "EVENT_TYPE_TICKETSHOP",
+      "numberOfTickets" => "1",
+      "paymentCurrency" => "EUR",
+      "paymentFee" => "846",
+      "paymentId" => "101234567",
+      "paymentIdentifier" => "3RWSXYO00008",
+      "paymentSalesTax" => "4656",
+      "paymentShipmentFee" => "0",
+      "paymentValue" => "24900",
+      "paymentVipSupportFee" => "0",
+      "secret" => "75857cffdb841b3d643071798589e996e2265b1c",
+      "ticketCategory0" => "Early Bird",
+      "ticketCategoryId0" => "3518340",
+      "ticketCategoryPrice0" => "24900",
+      "ticketEmail0" => "john.doe@example.invalid",
+      "ticketFirstName0" => "John",
+      "ticketId" => "123456789",
+      "ticketLastName0" => "Doe",
+      "ticketNumber0" => "3151-0123-9876"
+    }
   end
 
   describe "GET '/'" do
